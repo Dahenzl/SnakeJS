8de66e6639beb202f0c5ebee4a38efa4eefb6d06
David <ddhenriquez@uninorte.edu.co>
2023-01-30 19:50:41 -0500

    Implementing responsive divs

--- script.js
+++ script.js
@@ -4,9 +4,18 @@ function generateGrid() {
     for (let i = 0; i < 135; i++) {
         let item = document.createElement("div");
         item.classList.add("grid-item");
-        item.appendChild(document.createTextNode(i));
+        item.style.backgroundColor = randomColor();
         grid.appendChild(item);
     }
+}
+
+function randomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
 }
 
 generateGrid();
