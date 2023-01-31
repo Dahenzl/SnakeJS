80625811fcf35b9912cff516b38d461fcc3303d3
David <ddhenriquez@uninorte.edu.co>
2023-01-30 20:31:14 -0500

    First attempt of movement

--- script.js
+++ script.js
@@ -1,21 +1,58 @@
 let grid = document.querySelector(".grid");
+let position = 0;
+let direction = 3;
 
 function generateGrid() {
     for (let i = 0; i < 135; i++) {
         let item = document.createElement("div");
         item.classList.add("grid-item");
-        item.style.backgroundColor = randomColor();
         grid.appendChild(item);
     }
 }
 
-function randomColor() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
+function setDirection(e) {
+    switch (e.keyCode) {
+        case 65:
+            direction = 1;
+            break;
+        case 87:
+            direction = 2;
+            break;
+        case 68:
+            direction = 3;
+            break;
+        case 83:
+            direction = 4;
+            break;
     }
-    return color;
+}
+
+function movement() {
+    list[position].style.backgroundColor = "white";
+    switch (direction) {
+        case 1:
+            position--;
+            break;
+        case 2:
+            position -= 15;
+            break;
+        case 3:
+            position++;
+            break;
+        case 4:
+            position += 15;
+            break;
+        default:
+            break;
+    }
+    list[position].style.backgroundColor = "red";
 }
 
 generateGrid();
+let list = document.querySelectorAll(".grid-item");
+
+list[position].style.backgroundColor = "red";
+
+document.addEventListener("keydown", setDirection);
+
+setInterval(movement, 400);
