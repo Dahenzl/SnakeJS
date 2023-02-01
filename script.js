03a06e6d35e24d0e81300b0204edf40249fc58da
David <ddhenriquez@uninorte.edu.co>
2023-02-01 18:40:41 -0500

    Added apple generation and score system

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let grid = document.querySelector(".grid");
+let score = document.querySelector("#score");
 let position = 0;
 let direction = 0;
+let apple = 0;
 
 function generateGrid() {
     for (let i = 0; i < 135; i++) {
@@ -27,49 +29,53 @@ function setDirection(e) {
     }
 }
 
+function apple_generator() {
+    apple = Math.floor(Math.random() * 135);
+    list[apple].style.backgroundColor = "green";
+}
+
+function checkApple() {
+    if (position === apple) {
+        score.innerHTML = parseInt(score.innerHTML) + 10;
+        apple_generator();
+    }
+}
+
 function movement() {
     originalPosition = position
     switch (direction) {
         case 1:
             if (position % 15 === 0) {
-                position = 0;
-                direction = 3;
+                clearInterval(movementIntervalId);
             } else{
                 position --;
             }
             break;
         case 2:
             if (position < 15) {
-                position = 0;
-                direction = 3;
+                clearInterval(movementIntervalId);
             } else{
                 position -= 15;
             }
             break;
         case 3:
             if(position % 15 === 14){
-                position = 0;
-                direction = 3;
+                clearInterval(movementIntervalId);
             } else{
                 position ++;
             }
             break;
         case 4:
             if (position > 119) {
-                position = 0;
-                direction = 3;
+                clearInterval(movementIntervalId);
             } else{
                 position += 15;
             }
             break;
     }
-
-    if ((position < 0 || position > 134) || ((position+1)%15 == 0)) {
-        clearInterval(movementIntervalId);
-    } else {
-        list[originalPosition].style.backgroundColor = "white";
-        list[position].style.backgroundColor = "red";
-    }
+    list[originalPosition].style.backgroundColor = "white";
+    list[position].style.backgroundColor = "red";
+    checkApple();
 }
 
 generateGrid();
@@ -80,3 +86,4 @@ list[position].style.backgroundColor = "red";
 document.addEventListener("keydown", setDirection);
 
 movementIntervalId  = setInterval(movement, 400);
+apple_generator();
